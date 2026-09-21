0233c2aa188d0ea4b209869cf7f37ab4400820ca
jeanvertice09-a11y <jean.vertice09@gmail.com>
2026-09-21 13:23:15 -0300

    style(control): load responsive panel navigation styles

--- apps/web/src/routes/control.tsx
+++ apps/web/src/routes/control.tsx
@@ -22,6 +22,7 @@ import "../styles/console-system.css";
 import "../styles/console-compat.css";
 import "../styles/console-pages.css";
 import "../styles/editorial-console.css";
+import "../styles/panel-navigation-lot1.css";
 
 export const Route = createFileRoute("/control")({
   loader: async () => {
